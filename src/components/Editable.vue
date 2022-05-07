052fe014a25ea6d3382bf903ef8a8eae00a714b5
Rien Boydens <rien.boydens@gmail.com>
2022-05-07 21:46:58 +0200

    Basic profile page

---
+++ src/components/Editable.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+    emits: ['edit']
+}
+</script>
+<template>
+<div class="editable">
+    <img class="pencil" src="../assets/pencil.png" @click="$emit('edit')">
+    <slot />
+</div>
+</template>
+<style>
+.editable {
+    position: relative;
+}
+.editable > .pencil {
+    position: absolute;
+    top: 0px;
+    right: 0px;
+    width: 25px;
+    height: 25px;
+    border: 1px solid grey;
+    border-radius: 50%;
+    padding: 3px;
+    background: #fffa;
+}
+.editable > .pencil:hover {
+    background: #fffd;
+    cursor: pointer;
+}
+</style>
--- src/pages/Profile.vue
+++ src/pages/Profile.vue
@@ -1,8 +1,53 @@
 <script>
+import { getSolidDataset, getStringNoLocale, getThing, getUrl, getUrlAll } from "@inrupt/solid-client"
+import { FOAF, VCARD } from "@inrupt/vocab-common-rdf";
+import Editable from "../components/Editable.vue";
+
 export default {
-    props: ['webID'],
+    props: ["webID"],
+    data() {
+        return {
+            name: null,
+            nickname: null,
+            image: null,
+            friends: []
+        };
+    },
+    async beforeCreate() {
+        const profileDataset = await getSolidDataset(this.webID, {
+            fetch: this.session.fetch
+        });
+        const profile = getThing(profileDataset, this.webID);
+        this.name = getStringNoLocale(profile, VCARD.fn) || getStringNoLocale(profile, FOAF.name);
+        this.nickname = getStringNoLocale(profile, FOAF.nick);
+        this.image = getUrl(profile, "http://rdfs.org/sioc/ns#avatar") || getUrl(profile, VCARD.hasPhoto) || getUrl(profile, FOAF.img);
+        this.friends = getUrlAll(profile, FOAF.knows);
+    },
+    components: { Editable }
 }
 </script>
 <template>
-{{ webID }}
+<div class="profile-info">
+    <div class="col">
+        <Editable><h2 title="name">{{ name }}</h2></Editable>
+        <Editable><p title="nickname">{{ nickname }}</p></Editable>
+    </div>
+    <Editable><img width="100" height="100" :src="image"></Editable>
+</div>
 </template>
+<style>
+    .profile-info {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-around;
+    }
+
+    .profile-info > * {
+        margin: 3rem;
+    }
+
+    .col {
+        display: flex;
+        flex-direction: column;
+    }
+</style>
