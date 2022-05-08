179a3aed4cbb9c114097efee5613d0bcab8db4f1
Rien Boydens <rien.boydens@gmail.com>
2022-05-08 16:47:02 +0200

    Basic Overview page

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,32 @@
 <script setup>
 import Login from './pages/Login.vue';
+import Overview from './pages/Overview.vue';
 import Profile from './pages/Profile.vue';
 
 </script>
 
 <template>
-<Profile v-if="session.info.isLoggedIn" :webID="session.info.webId"></Profile>
+<Overview v-if="session.info.isLoggedIn" ></Overview>
 <Login v-else />
 </template>
 
 <style>
+* {
+  box-sizing: border-box;
+}
+
+html, body {
+  margin: 0;
+  padding: 0;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 0;
+  padding: 0;
 }
 </style>
---
+++ src/pages/Overview.vue
@@ -0,0 +1,53 @@
+<script>
+export default {
+    data() {
+        return {
+            size: 80,
+            image: "https://groep25.webdev.ilabt.imec.be/rien/profile/foto_gezicht.jpg",
+            chatrooms: ["Room 1", "Room 2", "Room 3"]
+        }
+    },
+    async beforeCreate() {
+
+    }
+}
+</script>
+<template>
+<div class="overview">
+    <div id="my-profile">
+        <img :src="image" :width="size" :height="size">
+        <h2>My Profile</h2>
+    </div>
+    <ul id="chatrooms">
+        <li v-for="chatroom in chatrooms">
+            <a><h2>{{ chatroom }}</h2></a>
+        </li>
+    </ul>
+</div>
+</template>
+<style>
+.overview {
+    width: 18rem;
+    height: 100vh;
+    float: left;
+    border: 4px solid black;
+    padding: 0;
+    margin: 0;
+}
+
+.overview > * {
+    padding: 5px;
+}
+
+#my-profile {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    align-items: center;
+    border-bottom: 1px solid black;
+}
+
+#chatrooms {
+    list-style: none;
+}
+</style>
