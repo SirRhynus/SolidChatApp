ad08a5891f082b418420228de742e7395e46f8bd
Rien Boydens <rien.boydens@gmail.com>
2022-07-28 11:18:16 +0200

    Removed VCard

--- src/pages/Profile.vue
+++ src/pages/Profile.vue
@@ -38,13 +38,13 @@ export default {
                 fetch: this.session.fetch
             });
             const profile = getThing(profileDataset, this.session.info.webId);
-            this.name = getStringNoLocale(profile, VCARD.fn) || getStringNoLocale(profile, FOAF.name);
+            this.name = getStringNoLocale(profile, FOAF.name);
             this.nickname = getStringNoLocale(profile, FOAF.nick);
-            this.image = getUrl(profile, SIOC.avatar) || getUrl(profile, VCARD.hasPhoto) || getUrl(profile, FOAF.img);
+            this.image = getUrl(profile, SIOC.avatar) || getUrl(profile, FOAF.img);
             this.friends = getUrlAll(profile, FOAF.knows);        
         },
         async submitName(e) {
-            await updateString(this.session.info.webId, VCARD.fn, this.name, { fetch: this.session.fetch });
+            await updateString(this.session.info.webId, FOAF.name, this.name, { fetch: this.session.fetch });
         },
         async submitNick(e) {
             const ds = await updateString(this.session.info.webId, FOAF.nick, this.nickname, { fetch: this.session.fetch });
